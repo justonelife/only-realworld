ff127bf7a618ddaa2b6810cb9817c0acf99c13c0
nguyennhut <nhut.nguyen@xomad.com>
2024-07-19 17:47:31 +0700

    feat(header): add class active when route change

--- src/app/shared/themes/header/header.component.ts
+++ src/app/shared/themes/header/header.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { AsyncPipe } from "@angular/common";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject } from "@angular/core";
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterLink } from "@angular/router";
+import { filter, tap } from "rxjs";
+
+interface PageRoute {
+  label: string,
+  routerLink: string,
+}
 
 @Component({
   selector: 'app-header',
@@ -8,16 +16,15 @@ import { RouterLink } from "@angular/router";
       <div class="container">
         <a class="navbar-brand" routerLink="/">conduit</a>
         <ul class="nav navbar-nav pull-xs-right">
-          <li class="nav-item">
-            <!-- Add "active" class when you're on that page" -->
-            <a class="nav-link active" routerLink="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" routerLink="/login">Sign in</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" routerLink="/register">Sign up</a>
-          </li>
+          @for (route of routes; track route.routerLink) {
+            <li class="nav-item">
+              <a [routerLink]="route.routerLink" 
+                class="nav-link"
+                [class.active]="activedRoute === route.routerLink">
+                {{ route.label }}
+              </a>
+            </li>
+          }
         </ul>
       </div>
     </nav>
@@ -28,4 +35,30 @@ import { RouterLink } from "@angular/router";
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  readonly router = inject(Router);
+  readonly cd = inject(ChangeDetectorRef);
+
+  routes: PageRoute[] = [
+    { label: 'Home', routerLink: '/home' },
+    { label: 'Sign in', routerLink: '/login' },
+    { label: 'Sign up', routerLink: '/register' },
+  ];
+
+  activedRoute: string = '';
+
+  constructor() {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      tap(e => {
+        if (e instanceof NavigationEnd) {
+          this.activedRoute = '/' + e.urlAfterRedirects.split('/')[1];
+          this.cd.markForCheck();
+        }
+      }),
+      takeUntilDestroyed(),
+    ).subscribe();
+  }
+
+
+}
